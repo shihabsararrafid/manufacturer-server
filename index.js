425f42ceffd014103212f8f9ba0129976943e9f8
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-05-29 08:05:19 +0600

    api to load data based on id done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,10 +20,20 @@ async function run() {
   try {
     await client.connect();
     const toolCollection = client.db("toolex").collection("tools");
+    // api for loading all data
     app.get("/tools", async (req, res) => {
       const query = {};
       const cursor = toolCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    // api to load single data based on id
+    app.get("/purchase/:id", async (req, res) => {
+      const itemId = req.params.id;
+      const query = { _id: ObjectId(itemId) };
+
+      const result = await toolCollection.findOne(query);
+      //console.log(itemId);
       res.send(result);
     });
   } finally {
