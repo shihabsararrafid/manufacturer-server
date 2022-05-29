469bbf309536e161f5da8c9fae785d900ed23a5c
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-05-29 07:18:52 +0600

    api for tools created

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+//coonecting with mongo db
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.43sj9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const toolCollection = client.db("toolex").collection("tools");
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const cursor = toolCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    ///
+  }
+}
+run().catch(console.dir);
 //
 app.get("/", (req, res) => {
   res.send("Running Successfully");
